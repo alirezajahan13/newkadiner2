fb528ce754b26130184d0d957a32079dd4477296
alirezajahan13 <107421412+alirezajahan13@users.noreply.github.com>
2023-12-09 22:56:35 +0330

    pages

--- js/kadinermain.js
+++ js/kadinermain.js
@@ -8,3 +8,18 @@ var swiper = new Swiper(".mySwiper", {
       el: ".swiper-pagination",
     },
 });
+$(document).ready(function(){
+  $('.faq > .question').click(function(){
+    $('.faq > .answer').slideUp(300);
+    if ($(this).siblings(".answer").css('display') == 'block') {
+        $(this).siblings('.answer').slideUp(500);
+        $('#openCloseFaq #closeFaq').hide(200);
+        $('#openCloseFaq #openFaq').show(200);
+    } else {
+        $(this).siblings('.answer').slideToggle(500);
+        
+        $('#openCloseFaq #openFaq').hide(200);
+        $('#openCloseFaq #closeFaq').show(200);
+    }
+  });
+});
---
+++ page-templates/rules-template.php
@@ -0,0 +1,42 @@
+<?php
+/*
+Template Name: Rules
+*/
+get_header();
+?>
+
+<div class="rulesPageBack">
+    <div class="rulesPage mainView highMargined">
+        <h1 class="smallHeading">قوانین همکاری کادینر با طراحان</h1>
+        <div class="rulesPageInner">
+            <div class="imageBox">
+                 <img src="<?php echo get_template_directory_uri()?>/img/aboutkd.png" alt="">
+            </div>
+            <div class="textBox">
+                <h3>قوانین همکاری کادینر با طراحان</h3>
+                <p>به طور کلی دو برنامه همکاری میان طراحان و کادینر وجود خواهد داشت؛ ( برای مشاهده متن توضیحات روی بخش مورد نظر کلیک کنید‌)</p>
+                <div class="faq darkBorder lowRadius mediumMargined">
+                    <div class="question">
+                        <span id="openCloseFaq">
+                            <svg id="openFaq" width="20" height="20" viewBox="0 0 18 18" xmlns="http://www.w3.org/2000/svg"><path d="M9 1v16m8-8H1" stroke="#000" stroke-width="2" fill="none" stroke-linecap="round" stroke-linejoin="round"/></svg>
+                            <svg id="closeFaq" width="20" height="20" viewBox="0 -8 18 18" xmlns="http://www.w3.org/2000/svg"><path stroke="#000" stroke-width="2" fill="none" stroke-linecap="round" stroke-linejoin="round" d="M1 1h16"/></svg>
+                        </span>
+                        <h3 class="noMargin lowPadding">فروش انحصاری</h3>
+                    </div>
+                    <p class="answer noMargin lowPadding">در برنامه فروش انحصاری مجموعه کادینر یک مجموعه‌ای را به طراح سفارش می‌دهد و کل پکیج طراحی را به قیمت مشخصی خریداری می‌کند، از آن پس مالکیت حقوقی تمام آثار با مجموعه کادینر است و طراح تنها مالکیت معنوی بر اثر دارد و اسم او به عنوان طراح ثبت خواهد شد، اما اختیاری در تصمیم‌گیری در زمینه نحوه فروش و استفاده از طرح نخواهد داشت.</p>
+                    <div class="question">
+                        <span id="openCloseFaq">
+                            <svg id="openFaq" width="20" height="20" viewBox="0 0 18 18" xmlns="http://www.w3.org/2000/svg"><path d="M9 1v16m8-8H1" stroke="#000" stroke-width="2" fill="none" stroke-linecap="round" stroke-linejoin="round"/></svg>
+                            <svg id="closeFaq" width="20" height="20" viewBox="0 -8 18 18" xmlns="http://www.w3.org/2000/svg"><path stroke="#000" stroke-width="2" fill="none" stroke-linecap="round" stroke-linejoin="round" d="M1 1h16"/></svg>
+                        </span>
+                        <h3 class="noMargin lowPadding">فروش اشتراکی</h3>
+                    </div>
+                    <p class="answer noMargin lowPadding">در فروش اشتراکی کلیت همکاری به این شکل است که طراح به ازای فروش هر طرح عدد ثابت و مشخصی را دریافت خواهد کرد. در زمینه فروش اشتراکی در بعضی طرح‌ها، طراح آثار گذشته خود را تحویل مجموعه کادینر می‌دهد تا کادینر به مانند یک پلتفرم آثار را به فروش برساند. در این شرایط طراح این اختیار را دارد که تصمیم بگیرد که در زمانی آثار او دیگر از طریق کادینر به فروش نرود. در این شرایط این آثار با برچسب «این اثر به درخواست طراح غیرقابل فروش شده است» نمایش داده خواهد شد. تبصره: با توجه به اینکه مجموعه‌ی کادینر هزینه‌هایی را در جهت آماده کردن طرح، قرار دادن در سایت و همچنین پرموت کردن طرح‌ها انجام می‌دهد، باید حداقل یک سال از زمان قرارگیری طرح در سایت گذشته باشد، تا طراح بتواند تصمیم بگیرد که طرح‌هایش از طریق مجموعه کادینر به فروش نرود. در بعضی مواقع مجموعه‌ای به سفارش کادینر و نظارت مستقیم مجموعه طراحی شده است و به خواسته طراح برنامه فروش اشتراکی فعال شده است. در این شرایط با توجه به سرمایه‌گذاری که مجموعه کادینر بر روی پکیج طراحی انجام داده و نظارت کاملی بر طراحی و اجرا داشته است، امکان تصمیم‌گیری در زمینه نحوه فروش برای طراح امکانپذیر نیست، اما طراح تا آخرین روز فعالیت مجموعه کادینر از عواید فروش آن بهره‌مند خواهد شد. همچنین در این شرایط مجموعه کادینر متعهد است تا از این طرح‌ها تنها در سایت کادینر استفاده کند.</p>
+                </div>
+                <p>کادینر امیدوار است تا همکاری خوبی با طراحان با استعداد ایران عزیزمان داشته باشد.</p>
+            </div>
+        </div>
+    </div>
+</div>
+
+<?php get_footer(); ?>
